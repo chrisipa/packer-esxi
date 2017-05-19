6981ab3131ed527049133a7e7a52656bca950aff
Christoph Papke <christoph.papke@kaba.com>
2017-05-19 22:45:36 +0200

    Added logging and bash completion and aliases.

--- templates/ubuntu-trusty/bootstrap.sh
+++ templates/ubuntu-trusty/bootstrap.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# configuration
+inputrcFile="/etc/inputrc"
+bashrcFile="/home/${osUsername}/.bashrc"
+
 # update all packages
 apt-get dist-upgrade -y
 
@@ -16,6 +20,16 @@ update-locale LANG=${osLocale}
 # set keyboard layout
 sed -i 's|'XKBMODEL=.*'|'XKBMODEL=pc105'|g' /etc/default/keyboard
 sed -i 's|'XKBLAYOUT=.*'|'XKBLAYOUT=${osKeyboardLayout}'|g' /etc/default/keyboard
+
+# add history completion
+echo "" >> "$inputrcFile"
+echo '"\e[5~": history-search-backward' >> "$inputrcFile"
+echo '"\e[6~": history-search-forward' >> "$inputrcFile"
+
+# add default aliases
+echo "" >> "$bashrcFile"
+echo "alias l='ls -altrh'" >> "$bashrcFile"
+echo "alias ..='cd ..'" >> "$bashrcFile"
 
 # show IP addresses
 ip addr show
